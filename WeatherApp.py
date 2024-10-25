b45977c8f0e81ac6e0e3b04573530462b2513e00
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-10-25 06:37:38 +0300

    Create WeatherApp.py

---
+++ WeatherApp.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import nominatim
+from timezonefinder import timezonefinder
+from datetime import *
+import requests
+import pytz
+from PIL import Image, ImageTk
+
+root = Tk()
+
+mainloop()
