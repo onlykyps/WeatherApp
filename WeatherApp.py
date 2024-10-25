ba2c7d5febb713da72c70147f4902629b2da02c4
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-10-25 06:46:36 +0300

    create weather app icon

--- WeatherApp.py
+++ WeatherApp.py
@@ -8,5 +8,14 @@ import pytz
 from PIL import Image, ImageTk
 
 root = Tk()
+root.title("Weather App")
+root.geometry("890x470+300+300")
+root.configure(bg="#57adff")
+root.resizable(False, False)
+
+#Icon
+image_icon = PhotoImage(file="Images/logo.png")
+root.iconphoto(False, image_icon)
 
 mainloop()
+
