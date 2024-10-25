75b43dfc10ef8dab2aa9babb08450a60ebe29c68
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-10-25 06:57:26 +0300

    Labels

--- WeatherApp.py
+++ WeatherApp.py
@@ -17,5 +17,38 @@ root.resizable(False, False)
 image_icon = PhotoImage(file="Images/logo.png")
 root.iconphoto(False, image_icon)
 
+#Labels
+label1 = Label(root, text="Temperature",
+               font=("Helvetica", 11),
+               fg="white",
+               bg="#203243")
+label1.place(x=50, y=120)
+
+label2 = Label(root, text="Humidity",
+               font=("Helvetica", 11),
+               fg="white",
+               bg="#203243")
+label2.place(x=50, y=140)
+
+label3 = Label(root, text="Pressure",
+               font=("Helvetica", 11),
+               fg="white",
+               bg="#203243")
+label3.place(x=50, y=160)
+
+label4 = Label(root, text="Wind Speed",
+               font=("Helvetica", 11),
+               fg="white",
+               bg="#203243")
+label4.place(x=50, y=180)
+
+label5 = Label(root, text="Description",
+               font=("Helvetica", 11),
+               fg="white",
+               bg="#203243")
+label5.place(x=50, y=200)
+
+Round_box = PhotoImage(file="Images/RoundRect_1.png")
+
 mainloop()
 
